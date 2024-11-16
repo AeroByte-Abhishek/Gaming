33da955c0594633dcb7d11fa746852c3791a84f7
AeroByte-Abhishek <167686628+AeroByte-Abhishek@users.noreply.github.com>
2024-11-16 11:27:37 +0530

    first commit

---
+++ Gaming-site/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-gray-900 text-white'>
+      <div>
+        app
+      </div>
+      <div className='m-20'>
+        gaming with abhishek
+      </div>
+    </div>
+  )
+}
+
+export default App
