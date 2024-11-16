6f32ec0d256ed2e6da02c0abf5d92de363ec3dfd
AeroByte-Abhishek <167686628+AeroByte-Abhishek@users.noreply.github.com>
2024-11-16 22:00:55 +0530

    Create landing page

--- Gaming-site/src/App.jsx
+++ Gaming-site/src/App.jsx
@@ -2,14 +2,35 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-gray-900 text-white'>
-      <div>
-        app
+    <>
+      <div className="relative w-full h-screen overflow-hidden">
+        {/* Video Background */}
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={videoSrc} type="video/mp4" />
+        </video>
+
+        {/* for better readability */}
+        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
+
+        {/* Text Content */}
+        <div className="relative z-10 flex flex-col items-end justify-center h-full px-14 text-white">
+          <h1 className="text-6xl mr-[2.5vw] font-bold leading-tight drop-shadow-4xl">
+            JOIN THE GAME
+          </h1>
+          <h3 className="mt-4 mr-[3vw] max-w-md text-gray-300 text-lg leading-relaxed">
+            Experience the thrill of immersive gameplay, epic battles, a
+            limitless fun. Whether you're a casual player or a hardcore gamer,
+            we've got something for everyone. Gear up, take control, and make
+            every moment legendary!
+          </h3>
+        </div>
       </div>
-      <div className='m-20'>
-        gaming with abhishek
-      </div>
-    </div>
+    </>
   )
 }
 
